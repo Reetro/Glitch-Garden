79948f2b7889fa6e90c661497eb8ce20a07ec639
Reetro <thegamerboy7@gmail.com>
2020-05-27 22:45:10 -0600

    Splash screen will now load main menu after 3 second delay

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] float menuDelay = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(StartScreenDelay());
+    }
+
+    IEnumerator StartScreenDelay()
+    {
+        yield return new WaitForSeconds(menuDelay);
+        LoadMainMenu();
+    }
+    
+    private void LoadMainMenu()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
