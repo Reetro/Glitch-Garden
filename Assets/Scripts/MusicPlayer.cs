7ff562c4e8cac78fe1cd3abe10f8ac14b001d05c
Reetro <thegamerboy7@gmail.com>
2020-05-27 22:32:12 -0600

    start work on a splash screen

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    void Awake()
+    {
+        int musicPlayerCount = FindObjectsOfType(GetType()).Length;
+
+        if (musicPlayerCount > 1)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
