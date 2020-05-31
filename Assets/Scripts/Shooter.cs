52efba0e82b9c1afe08003a521068059f6e62f22
Reetro <thegamerboy7@gmail.com>
2020-05-30 20:47:56 -0600

    started to setup a basic gun prefab

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField] GameObject projectile, gunPostion = null;
+    public void Fire()
+    {
+        Instantiate(projectile, gunPostion.transform.position, gunPostion.transform.rotation);
+    }
+}
