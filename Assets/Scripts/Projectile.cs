965c5d36969b58ca3451bbad3671c0de9e33ab33
Reetro <thegamerboy7@gmail.com>
2020-05-31 18:43:24 -0600

    projectile can now deal damage

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -5,8 +5,19 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     [SerializeField] float speed = 1f;
+    [SerializeField] float damage = 100f;
     void Update()
     {
         transform.Translate(Vector2.left * speed * Time.deltaTime);
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        var healthcomp = collision.GetComponent<Health>();
+
+        if (healthcomp)
+        {
+            healthcomp.DealDamage(damage);
+        }
     }
 }
