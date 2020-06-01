d7903c18eef12908107d52359e3566821512ada0
Reetro <thegamerboy7@gmail.com>
2020-06-01 09:45:06 -0600

    can now spawn defenders at mouse click location

---
+++ Assets/Scripts/DefenderSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject defender = null;
+    private void OnMouseDown()
+    {
+        var spawnLocation = GetSquareClick();
+
+        SpawnDefender(spawnLocation);
+    }
+
+    private void SpawnDefender(Vector2 SpawnLocation)
+    {
+        var newRotation = new Quaternion(0, 180, 0, 0);
+
+        GameObject newDefender = Instantiate(defender, SpawnLocation, newRotation) as GameObject;
+    }
+
+    private Vector2 GetSquareClick()
+    {
+        Vector2 clickPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(clickPos);
+
+        return worldPos;
+    }
+}
