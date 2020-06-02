869e60b75a5fa02773143197897902fd4cb4c602
Reetro <thegamerboy7@gmail.com>
2020-06-01 18:02:35 -0600

    defenders now snap to grid

--- Assets/Scripts/DefenderSpawner.cs
+++ Assets/Scripts/DefenderSpawner.cs
@@ -7,23 +7,30 @@ public class DefenderSpawner : MonoBehaviour
     [SerializeField] GameObject defender = null;
     private void OnMouseDown()
     {
-        var spawnLocation = GetSquareClick();
-
-        SpawnDefender(spawnLocation);
+        SpawnDefender(GetSquareClick());
     }
 
-    private void SpawnDefender(Vector2 SpawnLocation)
+    private void SpawnDefender(Vector2 spawnLocation)
     {
         var newRotation = new Quaternion(0, 180, 0, 0);
 
-        GameObject newDefender = Instantiate(defender, SpawnLocation, newRotation) as GameObject;
+        GameObject newDefender = Instantiate(defender, spawnLocation, newRotation) as GameObject;
     }
 
     private Vector2 GetSquareClick()
     {
         Vector2 clickPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
         Vector2 worldPos = Camera.main.ScreenToWorldPoint(clickPos);
+        Vector2 gridPos = SnapToGrid(worldPos);
 
-        return worldPos;
+        return gridPos;
+    }
+
+    private Vector2 SnapToGrid(Vector2 rawWorldPos)
+    {
+        float newX = Mathf.RoundToInt(rawWorldPos.x);
+        float newY = Mathf.RoundToInt(rawWorldPos.y);
+
+        return new Vector2(newX, newY);
     }
 }
