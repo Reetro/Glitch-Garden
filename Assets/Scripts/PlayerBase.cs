ff244460e910f6695a2f10fdefaa6fd3b68ee9fb
Reetro <thegamerboy7@gmail.com>
2020-06-03 16:22:52 -0600

    player can now lose if enough attackers get into the player base

---
+++ Assets/Scripts/PlayerBase.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBase : MonoBehaviour
+{
+    [SerializeField] int healthToRemove = 1;
+
+    PlayerHealthDisplay healthDisplay = null;
+
+    private void Start()
+    {
+        healthDisplay = FindObjectOfType<PlayerHealthDisplay>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject currentObject = collision.gameObject;
+
+        if (currentObject.GetComponent<Attacker>())
+        {
+            healthDisplay.RemoveHealth(healthToRemove);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerHealthDisplay.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+using TMPro;
+
+public class PlayerHealthDisplay : MonoBehaviour
+{
+    [SerializeField] int health = 5;
+
+    TextMeshProUGUI textDisplay = null;
+    LevelLoader levelLoader = null;
+
+    void Start()
+    {
+        textDisplay = GetComponent<TextMeshProUGUI>();
+
+        UpdateDisplay();
+    }
+
+    public void RemoveHealth(int amount)
+    {
+        health -= amount;
+        UpdateDisplay();
+
+        if (health <= 0)
+        {
+            LoadGameOver();
+        }
+    }
+
+    public void AddHealth(int amount)
+    {
+        health += amount;
+        UpdateDisplay();
+    }
+
+    public void UpdateDisplay()
+    {
+        textDisplay.text = health.ToString();
+    }
+
+    public void LoadGameOver()
+    {
+        SceneManager.LoadScene("Game Over");
+    }
+}
