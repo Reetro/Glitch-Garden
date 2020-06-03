262a6a019fdb13b7c4f87528b4449130126da70e
Reetro <thegamerboy7@gmail.com>
2020-06-03 12:46:44 -0600

    defenders can actually be attacked and die

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -7,13 +7,42 @@ public class Attacker : MonoBehaviour
 {
     [Range(0f, 5f)] float currentSpeed = 1f;
 
+    GameObject currentTarget = null;
+
     void Update()
     {
         transform.Translate(UnityEngine.Vector2.left * currentSpeed * Time.deltaTime);
+        UpdateAnimationState();
     }
 
     public void SetMovementSpeed(float speed)
     {
         currentSpeed = speed;
     }
+
+    public void Attack(GameObject target)
+    {
+        GetComponent<Animator>().SetBool("IsAttacking", true);
+        currentTarget = target;
+    }
+    public void StrikeTarget(float damage)
+    {
+        if (!currentTarget) { return; }
+
+        Health health = currentTarget.GetComponent<Health>();
+
+        if (health)
+        {
+            health.DealDamage(damage);
+        }
+    }
+
+    private void UpdateAnimationState()
+    {
+        if (!currentTarget)
+        {
+            GetComponent<Animator>().SetBool("IsAttacking", false);
+        }
+    }
+
 }
